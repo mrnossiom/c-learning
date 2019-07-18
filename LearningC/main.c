70da1f8a1d8d2f1ec53b026e83ed3354c0b22b22
Milo Moisson <milomoisson@gmail.com>
2019-07-18 18:15:14 +0200

    Initial Commit

---
+++ LearningC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  LearningC
+//
+//  Created by Milo Moisson on 18/07/2019.
+//  Copyright © 2019 Milo Moisson. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
